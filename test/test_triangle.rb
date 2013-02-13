7e52f1c1136d15e4615313ba2c46d9d40ae29f98
Kiyotaka Kumagai <kumakiyo@iij.ad.jp>
2013-02-13 17:10:53 +0900

    * update.

--- test/test_triangle.rb
+++ test/test_triangle.rb
@@ -4,26 +4,26 @@ require 'hostsan/triangle'
 class TestTriangle < Test::Unit::TestCase
  
   def test_is_regular_triangle
-    assert_equal(Triangle.determine(3, 3, 3), "正三角形です")
+    assert_equal(Hostsan::Triangle.determine(3, 3, 3), "正三角形です")
   end
  
   def test_is_isosceles_triangle
-    assert_equal(Triangle.determine(5, 3, 3), "二等辺三角形です")
+    assert_equal(Hostsan::Triangle.determine(5, 3, 3), "二等辺三角形です")
   end
 
   def test_is_triangle
-    assert_equal(Triangle.determine(4, 3, 2), "三角形です")
+    assert_equal(Hostsan::Triangle.determine(4, 3, 2), "三角形です")
   end
 
   def test_is_not_triangle_1
-    assert_equal(Triangle.determine(4, 2, 2), "三角形になりません")
+    assert_equal(Hostsan::Triangle.determine(4, 2, 2), "三角形になりません")
   end
 
   def test_is_not_triangle_2
-    assert_equal(Triangle.determine(4, 2, 0), "三角形になりません")
+    assert_equal(Hostsan::Triangle.determine(4, 2, 0), "三角形になりません")
   end
 
   def test_is_not_triangle_3
-    assert_equal(Triangle.determine(-4, 2, 5), "三角形になりません")
+    assert_equal(Hostsan::Triangle.determine(-4, 2, 5), "三角形になりません")
   end
 end
