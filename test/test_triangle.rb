57e2be7e646c97ff868af8ba7833e243fa88cd75
Kiyotaka Kumagai <kumakiyo@iij.ad.jp>
2013-02-13 16:52:33 +0900

    * update.

--- test/test_triangle.rb
+++ test/test_triangle.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'test_helper'
 require 'hostsan/triangle'
 class TestTriangle < Test::Unit::TestCase
@@ -6,4 +7,23 @@ class TestTriangle < Test::Unit::TestCase
     assert_equal(Triangle.determine(3, 3, 3), "正三角形です")
   end
  
+  def test_is_isosceles_triangle
+    assert_equal(Triangle.determine(5, 3, 3), "二等辺三角形です")
+  end
+
+  def test_is_triangle
+    assert_equal(Triangle.determine(4, 3, 2), "三角形です")
+  end
+
+  def test_is_not_triangle_1
+    assert_equal(Triangle.determine(4, 2, 2), "三角形になりません")
+  end
+
+  def test_is_not_triangle_2
+    assert_equal(Triangle.determine(4, 2, 0), "三角形になりません")
+  end
+
+  def test_is_not_triangle_3
+    assert_equal(Triangle.determine(-4, 2, 5), "三角形になりません")
+  end
 end
